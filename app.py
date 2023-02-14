89ba000a452214d2ab5412307c85da6acf28d689
Okuhle <okuhle.ndlebe@Okuhles-MacBook-Air.local>
2023-02-14 13:23:39 +0200

    initial commit

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 import streamlit as st
 import requests
 import json
+import mysql.connector
 
-st.title("OpenAI Chatbot")
-st.write("Interact with OpenAI's GPT-3 models in real-time using your OpenAI API. Choose from a selection of their best models, set the temperature and max tokens, and start a conversation. Delete the conversation at any time to start fresh.")
+st.title("Flowe to Chat Bastion")
+st.write("Start a conversation and delete the conversation at any time to start fresh.")
+
+# Connect to the database
+cnx = mysql.connector.connect(user='root', password='password',
+                              host='localhost', database='openai')
+cursor = cnx.cursor()
+
+# Create a table to store the conversation history
+cursor.execute("""
+     CREATE TABLE IF NOT EXISTS conversation_history (
+         id INT AUTO_INCREMENT PRIMARY KEY,
+         message TEXT,
+         is_user BOOLEAN
+     )
+ """)
 
 if "history" not in st.session_state:
-    st.session_state.history = []
+    # Retrieve the conversation history from the database
+    cursor.execute("SELECT message, is_user FROM conversation_history")
+    rows = cursor.fetchall()
+    st.session_state.history = [{"message": row[0], "is_user": row[1]} for row in rows]
 
 st.sidebar.markdown("## Configuration")
 KEY = st.sidebar.text_input("Enter Your OpenAI API Key", placeholder="API Key", value="")
@@ -17,13 +35,18 @@ temperature = st.sidebar.slider("Temperature", 0.0, 1.0, 0.7)
 max_tokens = st.sidebar.slider("Max Tokens", 0, 4000, 1786)
 
 if st.sidebar.button("Delete Conversation"):
+    # Clear the conversation history and delete the rows from the database
+    cursor.execute("TRUNCATE TABLE conversation_history")
     st.session_state.history = []
-st.sidebar.markdown("## GPT-3")
-st.sidebar.markdown("OpenAI's GPT-3 models can understand and generate natural language. They offer four main models with different levels of power suitable for different tasks. Davinci is the most capable model, and Ada is the fastest.")
-st.sidebar.markdown("text-davinci-003 | 4,000 max tokens")
-st.sidebar.markdown("text-curie-001 | 2,048 max tokens")
-st.sidebar.markdown("text-babbage-001 | 2,048 max tokens")
-st.sidebar.markdown("text-ada-001 | 2,048 max tokens")
+
+if st.sidebar.button("Display Conversation"):
+    for chat in st.session_state.history:
+        if chat['is_user']:
+            st.write("User: ", chat['message'])
+        else:
+            st.write("Bot: ", chat['message'])
+
+
 
 def generate_answer(prompt):
     API_KEY = KEY
@@ -50,8 +73,13 @@ def generate_answer(prompt):
         message_bot = result['choices'][0]['text'].strip()
         st.session_state.history.append({"message": prompt, "is_user": True})
         st.session_state.history.append({"message": message_bot, "is_user": False})
+        # Insert the new message into the database
+        cursor.execute("INSERT INTO conversation_history (message, is_user) VALUES (%s, %s)", (message_bot, False))
+        cnx.commit()
     else:
-        st.error("An error occurred while processing the API response. If using a model other than text-davinci-003, then lower the Max Tokens.")
+        st.error(
+            "An error occurred while processing the API response. If using a model other than text-davinci-003, then lower the Max Tokens.")
+
 
 prompt = st.text_input("Prompt", placeholder="Prompt Here", value="")
 if st.button("Submit"):
